deebe5b1a3ddd63fc3ba593db9e1503a573fbb11
JinWook <jwook0318@gmail.com>
2025-11-14 15:23:24 +0900

    feat(config): eslint, prettier config setting

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,53 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
 import { defineConfig, globalIgnores } from 'eslint/config'
 import tanstackQuery from '@tanstack/eslint-plugin-query'
+import eslintReact from '@eslint-react/eslint-plugin';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true},
+      },
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      '@tanstack/query': tanstackQuery,
+      prettier: prettierPlugin
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      reactPlugin.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintReact.configs['recommended-typescript'],
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      '@tanstack/query': tanstackQuery,
-    },
     rules: {
-      '@tanstack/query/exhaustive-deps': 'error',
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'no-console': 'error',
+      'no-unused-vars': 'error',
+      'react/jsx-no-target-blank': 'off',
     },
   },
+  prettierConfig
 ])
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'node:path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [react(), tailwindcss()],
+});
